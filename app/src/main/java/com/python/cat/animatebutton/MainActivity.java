5e9c78cfb11d31e0478bc8480dccbb6e4c01322d
pythoncat <pythoncat@qq.com>
2017-05-04 20:36:33 +0800

    first init

---
+++ app/src/main/java/com/python/cat/animatebutton/MainActivity.java
@@ -0,0 +1,13 @@
+package com.python.cat.animatebutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
