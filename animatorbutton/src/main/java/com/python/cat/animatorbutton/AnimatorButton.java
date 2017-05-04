52c5a0542aac5b20bf49a67a4951a217e1558ce2
pythoncat <pythoncat@qq.com>
2017-05-04 21:36:59 +0800

    + animate  rect 2 roundRect ok

--- animatorbutton/src/main/java/com/python/cat/animatorbutton/AnimatorButton.java
+++ animatorbutton/src/main/java/com/python/cat/animatorbutton/AnimatorButton.java
@@ -1,6 +1,13 @@
 package com.python.cat.animatorbutton;
 
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -22,6 +29,18 @@ public class AnimatorButton extends View {
         LogUtils.getLogConfig().configShowBorders(false);
     }
 
+    private Paint outerPaint;
+    private Paint textPaint;
+
+    private String textStr = "确认完成";
+    private RectF outerRectF;
+    private PointF centerPoint;
+    private int radiusMax;
+    private float curRadius;
+    private int rect2CircleDuration = 1000;
+    private ValueAnimator rect2CircleAnimator;
+    private AnimatorSet animatorSet;
+
     public AnimatorButton(Context context) {
         this(context, null);
     }
@@ -35,15 +54,60 @@ public class AnimatorButton extends View {
         init();
     }
 
+    private void init() {
+        outerPaint = new Paint();
+        outerPaint.setColor(Color.parseColor("#3F51B5"));// TODO -->
+        outerPaint.setStyle(Paint.Style.FILL);
+        outerRectF = new RectF();
+        animatorSet = new AnimatorSet();
+    }
+
+    private void setAnimator() {
+        setRoundRect();
+        animatorSet
+                .play(rect2CircleAnimator);
+    }
+
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         this.width = w;
         this.height = h;
-        LogUtils.e("width = " + width + " , height = " + height);
+        LogUtils.w("width = " + width + " , height = " + height);
+        outerRectF.left = 0;
+        outerRectF.right = width;
+        outerRectF.top = 0;
+        outerRectF.bottom = height;
+        centerPoint = new PointF();
+        centerPoint.x = Math.round(outerRectF.right / 2.0 - outerRectF.left / 2.0);
+        centerPoint.y = Math.round(outerRectF.bottom / 2.0 - outerRectF.top / 2.0);
+        LogUtils.d("centerPoint.x = " + centerPoint.x + " , .y = " + centerPoint.y);
+        radiusMax = Math.min(width, height) / 2;
+        LogUtils.d("radiusMax = " + radiusMax);
+        setAnimator();
     }
 
-    private void init() {
-
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawRoundRect(outerRectF, curRadius, curRadius, outerPaint);
     }
+
+    private void setRoundRect() {
+        rect2CircleAnimator = ValueAnimator.ofInt(0, radiusMax);
+        rect2CircleAnimator.setDuration(rect2CircleDuration);
+        rect2CircleAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                LogUtils.i("update curRadius--> " + animation.getAnimatedValue());
+                curRadius = (int) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+    }
+
+    public void start() {
+        animatorSet.start();
+    }
+
 }
--- app/src/main/java/com/python/cat/animatebutton/MainActivity.java
+++ app/src/main/java/com/python/cat/animatebutton/MainActivity.java
@@ -2,12 +2,25 @@ package com.python.cat.animatebutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.python.cat.animatorbutton.AnimatorButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AnimatorButton ab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ab = (AnimatorButton) findViewById(R.id.btn_animate);
+        ab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ab.start();
+            }
+        });
+
     }
 }
